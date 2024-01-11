dc7429e56bbefc7678dc0b8b7f8cf2ee78c83f37
IDON-TEXIST <waddwasad@gmail.com>
2024-01-11 15:54:31 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+from requests import ReadTimeout
 import pywikibot
 import requests
 import re
@@ -11,26 +12,30 @@ IOC_PROFILE = matchBuilder.pattern("IOC profile")
 OLYMPICS_COM_PROFILE = [matchBuilder.pattern("Olympics.com profile"), matchBuilder.pattern("Olympic Channel"), matchBuilder.pattern("Olympics.com")]
 EDIT_SUMMARY = "{{[[Template:IOC profile|IOC profile]]}} is being merged into {{[[Template:Olympics.com profile|Olympics.com profile]]}} "
 EDIT_SUMMARY += "([[Wikipedia:Templates for discussion/Log/2021 May 6#Template:Olympic Channel|TfD]]) "
-EDIT_SUMMARY += "([[Wikipedia:Bots/Requests for approval/Yet another TfD implementor bot|Bot trial]])"
+EDIT_SUMMARY += "([[Wikipedia:Bots/Requests for approval/Yet another TfD implementor bot|BRfA]])"
 
-def processPage(page : pywikibot.Page):
+factory = pagegenerators.GeneratorFactory()
+factory.handle_arg("-transcludes:IOC profile")
+pageset = factory.getCombinedGenerator()
+for page in pageset:
     if pywikibot.Page(site, "User:Yet another TfD implementor bot/Switch").text != "OK":
-        return
+        print("Bot disabled by switch")
+        exit()
 
     for regex in OLYMPICS_COM_PROFILE:
         if (re.search(regex, page.text) != None): # This template has the same merge target and would be a duplicate
             page.text = re.sub("\n.*\{\{IOC profile.*?\}\}", "", page.text)
-
             try:
-                page.save(EDIT_SUMMARY, minor=False)
+                page.save(EDIT_SUMMARY, minor=False, botflag=True)
+                time.sleep(30)
             except pywikibot.exceptions.LockedPageError:
-                print(page.title + " is protected")
+                print(page.title() + " is protected")
             finally:
-                return
+                continue
         
     match = re.search(IOC_PROFILE, page.text)
     if match is None:
-        return # This script only processes redirects one at a time because it's easier
+        continue # This script only processes redirects one at a time because it's easier
     
     # Search the page text for {{IOC profile}}, use extract_templates_and_params() to parse the params, 
     # grab the index 0 template from the list as it is going to be the only one, and then the first
@@ -46,9 +51,9 @@ def processPage(page : pywikibot.Page):
         wikidataItem = pywikibot.ItemPage.fromPage(page)
         wikidataItem.get()
         if not wikidataItem.claims:
-            return # All possible locations of the ID have been exhausted
+            continue # All possible locations of the ID have been exhausted
         if not "P3171" in wikidataItem.claims:
-            return
+            continue
         id = wikidataItem.claims["P3171"][0].getTarget()
     if "name" in params:
         name = params["name"]
@@ -56,22 +61,20 @@ def processPage(page : pywikibot.Page):
         name = params["2"]
     
     # olympics.com seems to block automated traffic, so we get the target from the headers instead without connecting to them.
-    newid = requests.head("https://www.olympic.org/" + id, timeout=5).headers['Location'][33:]
+    try:
+        newid = requests.head("https://www.olympic.org/" + id, timeout=5).headers['Location'][33:]
+    except ReadTimeout:
+        print("Editing " + page.title() + " failed because of timeout. " + id)
+        continue
     if name == None:
         page.text = re.sub(IOC_PROFILE, "{{Olympics.com profile|" + newid + "}}", page.text)
     else:
         page.text = re.sub(IOC_PROFILE, "{{Olympics.com profile|" + newid + "|" + name + "}}", page.text)
     
     try:
-        page.save(EDIT_SUMMARY, minor=False)
+        page.save(EDIT_SUMMARY, minor=False, botflag=True)
+        time.sleep(30)
     except pywikibot.exceptions.LockedPageError:
         print(page.title + " is protected")
     finally:
-        return
-
-factory = pagegenerators.GeneratorFactory()
-factory.handle_arg("-transcludes:IOC profile")
-pageset = factory.getCombinedGenerator()
-for i in pageset:
-    processPage(pageset)
-    time.sleep(30)
+        continue
