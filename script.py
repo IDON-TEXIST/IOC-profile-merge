e0e7a2a3d3ad9fcb1998d0fe31e8165b31f4bcd7
IDON-TEXIST <waddwasad@gmail.com>
2024-01-11 17:02:00 -0500

    what an annoying edge case

--- script.py
+++ script.py
@@ -8,15 +8,17 @@ from pywikibot import pagegenerators
 site = pywikibot.Site()
 
 matchBuilder = pywikibot.textlib.MultiTemplateMatchBuilder(site)
-IOC_PROFILE = matchBuilder.pattern("IOC profile")
+REPLACED_TEMPLATE = "IOC profile"
+IOC_PROFILE = matchBuilder.pattern(REPLACED_TEMPLATE)
 OLYMPICS_COM_PROFILE = [matchBuilder.pattern("Olympics.com profile"), matchBuilder.pattern("Olympic Channel"), matchBuilder.pattern("Olympics.com")]
+SPORTS_LINKS = [matchBuilder.pattern("Sports links"), matchBuilder.pattern("Sport links"), matchBuilder.pattern("Sport link")]
 EDIT_SUMMARY = "{{[[Template:IOC profile|IOC profile]]}} is being merged into {{[[Template:Olympics.com profile|Olympics.com profile]]}} "
 EDIT_SUMMARY += "([[Wikipedia:Templates for discussion/Log/2021 May 6#Template:Olympic Channel|TfD]]) "
 EDIT_SUMMARY += "([[Wikipedia:Bots/Requests for approval/Yet another TfD implementor bot|BRfA]])"
 HEADERS = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
 
 factory = pagegenerators.GeneratorFactory()
-factory.handle_arg("-transcludes:IOC profile")
+factory.handle_arg("-transcludes:" + REPLACED_TEMPLATE)
 pageset = factory.getCombinedGenerator()
 for page in pageset:
     if pywikibot.Page(site, "User:Yet another TfD implementor bot/Switch").text != "OK":
@@ -25,7 +27,7 @@ for page in pageset:
 
     for regex in OLYMPICS_COM_PROFILE:
         if (re.search(regex, page.text) != None): # This template has the same merge target and would be a duplicate
-            page.text = re.sub("\n.*\{\{IOC profile.*?\}\}", "", page.text)
+            page.text = re.sub("\n.*\{\{" + REPLACED_TEMPLATE + ".*?\}\}", "", page.text)
             try:
                 page.save(EDIT_SUMMARY, minor=False, botflag=True)
                 time.sleep(30)
@@ -33,6 +35,21 @@ for page in pageset:
                 print(page.title() + " is protected")
             finally:
                 continue
+
+    for regex in SPORTS_LINKS:
+        if (re.search(regex, page.text) != None):
+            wikidataItem = pywikibot.ItemPage.fromPage(page)
+            wikidataItem.get()
+            if wikidataItem.claims:
+                if "P3171" in wikidataItem.claims:
+                    page.text = re.sub("\n.*\{\{" + REPLACED_TEMPLATE + ".*?\}\}", "", page.text)
+                    try:
+                        page.save(EDIT_SUMMARY, minor=False, botflag=True)
+                        time.sleep(30)
+                    except pywikibot.exceptions.LockedPageError:
+                        print(page.title() + " is protected")
+                    finally:
+                        continue
         
     match = re.search(IOC_PROFILE, page.text)
     if match is None:
